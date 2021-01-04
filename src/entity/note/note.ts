b4154e925305d5d5816a93cdd0ec42269e8bcee1
Albert Oboh <albertoboh7@gmail.com>
2021-01-04 09:48:43 +0100

    create note entity

---
+++ src/entity/note/note.ts
@@ -0,0 +1,41 @@
+import { type } from "os";
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { User } from "../user/user";
+
+@Entity("notes")
+export class Note {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @ManyToOne((type) => User, (user) => user.note) owner: User;
+
+  @ManyToMany((type) => User)
+  @JoinTable()
+  members: User[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
