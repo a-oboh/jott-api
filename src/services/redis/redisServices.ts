ee2ae9afdc0d5f9b9e69cfbbafb753dee31e9cf4
Albert Oboh <albertoboh7@gmail.com>
2021-01-14 23:36:37 +0100

    minor changes

--- src/services/redis/redisServices.ts
+++ src/services/redis/redisServices.ts
@@ -1,14 +1,19 @@
 import { promisifyAll } from "bluebird";
 const { promisify } = require("util");
 import redis from "redis";
-import { redisClient as client } from "../../app";
+import { currentConfig as config } from "../../config/index";
+// import { redisClient } from "../../app";
 
 export class RedisService {
   constructor() {}
 
-  setAsync = promisify(client.set).bind(client);
-  setExAsync = promisify(client.setex).bind(client);
-  getAsync = promisify(client.get).bind(client);
+  REDIS_PORT = config.app.redisPort || 6379;
+
+  redisClient = redis.createClient({ port: this.REDIS_PORT });
+
+  private setAsync = promisify(this.redisClient.set).bind(this.redisClient);
+  private setExAsync = promisify(this.redisClient.setex).bind(this.redisClient);
+  private getAsync = promisify(this.redisClient.get).bind(this.redisClient);
 
   async setValue(key: string, val: string) {
     // promisifyAll(client);
@@ -16,7 +21,7 @@ export class RedisService {
     return result;
   }
 
-  async setTtlValue(key: string, val: string, ttl: number) { 
+  async setValueTtl(key: string, val: string, ttl: number) {
     // promisifyAll(client);
     const result = await this.setExAsync(key, ttl, val);
     return result;
