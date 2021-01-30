3145104dc15d24e11d326052932ed981180b8c40
Albert Oboh <albertoboh7@gmail.com>
2021-01-31 00:15:27 +0100

    add get all nnotes and single notes

--- src/controllers/noteController.ts
+++ src/controllers/noteController.ts
@@ -5,6 +5,7 @@ import { UserService } from "../services/userService";
 import { NoteService } from "../services/notes/noteService";
 import { RedisService } from "../services/redis/redisServices";
 import { cacheRequest } from "helpers/cache";
+import paginate from "helpers/paginate";
 
 export class NoteController {
   constructor() {}
@@ -13,7 +14,77 @@ export class NoteController {
   // noteService: NoteService;
   // redisService: RedisService;
 
-  async getNotes(req: Request, res: Response, next: NextFunction) {}
+  async getNotes(req: Request, res: Response, next: NextFunction) {
+    const userService = new UserService();
+    const noteService = new NoteService();
+    const redisService = new RedisService();
+
+    try {
+      const userId = req.params.userId;
+
+      const user = await userService.getUserById(userId);
+
+      const notes = await noteService.getNotesByOwner(user);
+
+      // get page and limit for pagination
+      const { page = `1`, limit = `10` } = req.query;
+
+      //paginate data
+      const result = paginate(<[]>notes, <string>page, <string>limit);
+
+      return res.json({
+        status: "success",
+        message: "notes retrieved successfully",
+        data: result.result,
+        nextPage: result.nextPage,
+        previousPage: result.previousPage,
+        limit: result.limitNum,
+        count: result.result.length,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  async getSingleNote(req: Request, res: Response, next: NextFunction) {
+    const userService = new UserService();
+    const noteService = new NoteService();
+    const redisService = new RedisService();
+
+    try {
+      const { userId, noteId } = req.params;
+
+      //check if user note cached
+      const resSaved = await redisService.getValue(`note_${noteId}`);
+
+      if (resSaved) {
+        const result = JSON.parse(resSaved);
+        return res.send({
+          status: "success",
+          message: "note retrieved successfully",
+          data: result.data,
+        });
+      }
+
+      const user = await userService.getUserById(userId);
+
+      const note = await noteService.getSingleNote(user, noteId);
+
+      await cacheRequest(
+        `note_${note.id}`,
+        JSON.stringify({ data: note }),
+        600
+      );
+
+      return res.json({
+        status: "success",
+        message: "note retrieved successfully",
+        data: note,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  }
 
   async createNote(req: Request, res: Response, next: NextFunction) {
     if (!req.body) {
@@ -33,7 +104,7 @@ export class NoteController {
         );
       }
 
-      //check if data is cached
+      //check if the request has been sent before
       const resSaved = await redisService.getValue(idempotentKey.toString());
 
       var obj = JSON.parse(resSaved);
@@ -46,12 +117,12 @@ export class NoteController {
         });
       }
 
-      // const {title, content, ownerId} = req.body;
       const ownerId: string = req.user.id;
       const title: string = req.body.title;
       const content: string = req.body.content;
 
       const user = await userService.getUserById(ownerId);
+      user.password = undefined;
 
       const noteToSave = new Note();
 
@@ -70,12 +141,10 @@ export class NoteController {
       //store idempotent key and response in cache
       await cacheRequest(idempotentKey.toString(), JSON.stringify(response));
 
-      res.status(201).json(response);
+      await cacheRequest(`note_${newNote.id}`, JSON.stringify(response), 600);
+
+      return res.status(201).json(response);
     } catch (err) {
-      // return res.status(500).send({
-      //   status: "error",
-      //   message: err.message || `error creating new note`,
-      // });
       return next(err);
     }
   }
@@ -83,8 +152,6 @@ export class NoteController {
   async updateNote(req: Request, res: Response, next: NextFunction) {
     if (!req.body) {
       return next(new HttpError("request body cannot be empty", 400));
-    } else if (!req.params) {
-      return next(new HttpError("note id cannot be empty", 400));
     }
 
     try {
@@ -92,12 +159,6 @@ export class NoteController {
       let noteService = new NoteService();
 
       const idempotentKey = req.headers["x-idempotent-key"];
-
-      if (!idempotentKey) {
-        return next(
-          new HttpError("idempotent key header cannot be empty", 400)
-        );
-      }
 
       //check if data is cached
       const resSaved = await redisService.getValue(idempotentKey.toString());
@@ -133,7 +194,13 @@ export class NoteController {
       //store idempotent key and response in cache
       await cacheRequest(idempotentKey.toString(), JSON.stringify(response));
 
-      res.status(200).json(response);
+      await cacheRequest(
+        `note_${updatedNote.id}`,
+        JSON.stringify(response),
+        600
+      );
+
+      return res.status(200).json(response);
     } catch (err) {
       return next(err);
     }
