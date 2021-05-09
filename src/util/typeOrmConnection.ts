337715f1176a7b133da6d1b3c2f8dcdc6e79e226
Albert Oboh <albertoboh7@gmail.com>
2021-05-09 01:05:16 +0100

    add DB utils

--- src/util/typeOrmConnection.ts
+++ src/util/typeOrmConnection.ts
@@ -5,7 +5,6 @@ import {
   getConnectionOptions,
   getRepository,
 } from "typeorm";
-import bcrypt from "bcrypt";
 
 const createTypeOrmConnection = async () => {
   //override connection options
@@ -23,7 +22,7 @@ const createTestUsers = async () => {
   newUser.email = "user@test.com";
   newUser.firstName = "Ayo";
   newUser.lastName = "Balogun";
-  newUser.password = await bcrypt.hash("secret", 10);
+  newUser.password = "secret";
 
   const userRepository = getRepository(User);
 
@@ -34,4 +33,29 @@ const createTestUsers = async () => {
   console.log('saved user')
 };
 
-export { createTypeOrmConnection, createTestUsers, closeConnection };
+ const cleanDb = async () => {
+  const entities = await getEntities();
+  await cleanAll(entities);
+}
+
+const getEntities = async () => {
+  const entities = [];
+    (await (await getConnection()).entityMetadatas).forEach(
+      x => entities.push({name: x.name, tableName: x.tableName})
+    );
+    return entities;
+}
+
+ const cleanAll = async (entities) => {
+  try {
+    for (const entity of entities) {
+      const repository = await getRepository(entity.name);
+      await repository.query(`TRUNCATE TABLE \`${entity.tableName}\`;`);
+    }
+  } catch (error) {
+    throw new Error(`ERROR: Cleaning test db: ${error}`);
+  }
+}
+
+
+export { createTypeOrmConnection, createTestUsers, closeConnection, cleanDb };
